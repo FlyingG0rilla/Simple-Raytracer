c714d602db237db1165378141dc50e8c4547fba4
FlyingGorila <schwabdennis.privat@web.de>
2025-12-28 18:40:12 +0100

    save local changes

---
+++ cmake-build-debug/CMakeFiles/ComputergrafikLabor_A3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ComputergrafikLabor_A3.dir/geometry.cc.obj"
+  "CMakeFiles/ComputergrafikLabor_A3.dir/geometry.cc.obj.d"
+  "CMakeFiles/ComputergrafikLabor_A3.dir/math.cc.obj"
+  "CMakeFiles/ComputergrafikLabor_A3.dir/math.cc.obj.d"
+  "CMakeFiles/ComputergrafikLabor_A3.dir/raytracer.cc.obj"
+  "CMakeFiles/ComputergrafikLabor_A3.dir/raytracer.cc.obj.d"
+  "ComputergrafikLabor_A3.exe"
+  "ComputergrafikLabor_A3.exe.manifest"
+  "ComputergrafikLabor_A3.pdb"
+  "libComputergrafikLabor_A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ComputergrafikLabor_A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
